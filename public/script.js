8332aa5a13ee88919a5dcaefab92f0f88c8ce7b0
Bradley Huang <ultravenusk1@gmail.com>
2023-03-02 23:40:16 -0500

    icon

--- public/script.js
+++ public/script.js
@@ -5,7 +5,8 @@ $(window).on("load",function() {
         var objectBottom = $(this).offset().top + $(this).outerHeight();
         if (objectBottom < windowBottom) {
           if ($(this).css("opacity")==0) {$(this).fadeTo(200,1);}
-        } else { 
+          
+        }else{
           if ($(this).css("opacity")==1) {$(this).fadeTo(200,0);}
         }
       });
