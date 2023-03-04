0cb69189533be4c46ec55498585da8b1375f0233
Bradley Huang <ultravenusk1@gmail.com>
2023-03-04 03:21:46 -0500

    huge

--- public/script.js
+++ public/script.js
@@ -4,10 +4,26 @@ $(window).on("load",function() {
       $(".block").each(function() {
         var objectBottom = $(this).offset().top + $(this).outerHeight();
         if (objectBottom < windowBottom+50) {
-          if ($(this).css("opacity")==0) {$(this).fadeTo(200,1);}
+          if ($(this).css("opacity")==0.1) {$(this).fadeTo(200,1);}
           
         }else{
-          if ($(this).css("opacity")==1) {$(this).fadeTo(200,0);}
+          if ($(this).css("opacity")==1) {$(this).fadeTo(200,0.1);}
+        }
+      });
+    }).scroll();
+
+
+    $(window).scroll(function() {
+      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+      $("#phone").each(function() {
+        var objectBottom = $(this).offset().top + $(this).outerHeight();
+        var hrBottom = $("#divide").offset().top + $("#divide").outerHeight();
+        if (objectBottom > hrBottom) {
+          if ($(this).css("opacity")==1) {$(this).fadeTo(100,0);}
+          if ($("#third").css("opacity")==1) {$("#third").fadeTo(300,0.1);}
+        }else{
+          if ($(this).css("opacity")==0) {$(this).fadeTo(100,1);}
+          if ($("#third").css("opacity")==0.1) {$("#third").fadeTo(300,1);}
         }
       });
     }).scroll();
